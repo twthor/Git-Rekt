7231a818a36568ff5de32496886032e57a52fa78
Mina Tolfsen <minabye@me.com>
2024-02-22 14:44:51 +0100

    Laget mappe til «map» i model mappen. Laget klasser under mappen «map» og skrevet i hver klasse hva iden og tanken rundt hver klasses er.

---
+++ src/main/java/inf112/moustachmania/app/model/map/IMap.java
@@ -0,0 +1,6 @@
+package inf112.moustachmania.app.model.map;
+
+// lage en interface for mappene og skal lage metoder for å hente ut informasjon om mappene, som f.eks. størrelse, og layers
+public interface IMap {
+
+}
