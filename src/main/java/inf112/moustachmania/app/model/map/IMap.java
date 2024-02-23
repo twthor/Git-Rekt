4522f2125c78d418e815387ca35a4706dd909023
Mina Tolfsen <minabye@me.com>
2024-02-23 11:38:25 +0100

    Begynt å kode i IMap, og planlagt strukturer for mappen «map»

--- src/main/java/inf112/moustachmania/app/model/map/IMap.java
+++ src/main/java/inf112/moustachmania/app/model/map/IMap.java
@@ -1,6 +1,23 @@
 package inf112.moustachmania.app.model.map;
 
+import java.awt.*;
+
 // lage en interface for mappene og skal lage metoder for å hente ut informasjon om mappene, som f.eks. størrelse, og layers
 public interface IMap {
+
+    /**
+     * Get the layer of the map
+     * @param layerName the name of the layer
+     * @param mapPath the path to the map
+     * @return a 2D boolean array of the layer
+     */
+    boolean [][] getLayer(String layerName, String mapPath);
+
+    /**
+     * Get the dimensions of the map
+     * @param mapPath the path to the map
+     * @return the dimensions of the map
+     */
+    Point getMapDimensions(String mapPath);
 
 }
