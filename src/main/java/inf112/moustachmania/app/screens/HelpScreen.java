f426f04497935fb161ed4c19f448cbc123f60706
Emil Johannessen <vud017@uib.no>
2024-03-05 14:20:37 +0100

    pause-screen implemented with descriptions and background test picture

--- src/main/java/inf112/moustachmania/app/screens/HelpScreen.java
+++ src/main/java/inf112/moustachmania/app/screens/HelpScreen.java
@@ -26,8 +26,11 @@ public class HelpScreen implements Screen {
         this.game = game;
         this.texturesToDispose = new ArrayList<>();
 
+        addBackgroundImage("assets/testPicture.png");
+
         setupUiTable();
-        Gdx.input.setInputProcessor(stage);
+
+        show();
     }
 
     private void setupUiTable() {
@@ -41,7 +44,6 @@ public class HelpScreen implements Screen {
 
         setupBackTostartScreenButton(uiRoot);
 
-        addControlImage(uiRoot, "assets/arrowkeys.png");
     }
 
     // set up description of the controls and lables to controlsTable
@@ -51,32 +53,36 @@ public class HelpScreen implements Screen {
 
         // Creating Labels description for controls
         Label titleLabel = new Label("Game Controls:", game.getSkin());
-        Label moveLabel = new Label("Move: Arrow keys <-  -> ", game.getSkin());
-        Label jumpLabel = new Label("Jump: Spacebar ---- ", game.getSkin());
-        Label pauseGameLabel = new Label("Pause game: Esc ", game.getSkin());
+        Label moveLabel = new Label("Move: Arrow keys  <-  -> ", game.getSkin());
+        Label jumpLabel = new Label("Jump: Spacebar  ===== ", game.getSkin());
+        Label pauseGameLabel = new Label("Pause game:  esc ", game.getSkin());
 
         // Scaling the size of the labels
-        titleLabel.setFontScale(2f);
+        titleLabel.setFontScale(2.5f);
         moveLabel.setFontScale(1.5f);
         jumpLabel.setFontScale(1.5f);
         pauseGameLabel.setFontScale(1.5f);
 
         // Add the labels to the controlsTable
-        controlsTable.add(titleLabel).padBottom(30).padTop(30).row();
-        controlsTable.add(moveLabel).padBottom(10).row();
-        controlsTable.add(jumpLabel).padBottom(10).row();
-        controlsTable.add(pauseGameLabel).row();
+        controlsTable.add(titleLabel).padBottom(20).padTop(10).center().row();
+        controlsTable.add(moveLabel).padBottom(10).left().row();
+        controlsTable.add(jumpLabel).padBottom(10).left().row();
+        controlsTable.add(pauseGameLabel).padBottom(10).left().row();
 
-        uiRoot.add(controlsTable).expand().top();
+        uiRoot.add(controlsTable).expand().fill();
     }
 
-    private void addControlImage(Table table, String imagePath) {
+    /*
+    Adds the background image
+     */
+    private void addBackgroundImage(String imagePath) {
         Texture imageTexture = new Texture(Gdx.files.internal(imagePath));
         Image image = new Image(imageTexture);
-        table.add(image).center().padBottom(20).row();
+        image.setSize(stage.getWidth(), stage.getHeight());
+        stage.addActor(image);
+
         texturesToDispose.add(imageTexture);
     }
-
 
     // set up the back-button and click-handling
     private void setupBackTostartScreenButton(Table uiRoot) {
@@ -84,15 +90,16 @@ public class HelpScreen implements Screen {
         helpButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                //handleNewGameButtonEvent();
                 game.startScreen = new StartScreen(game);
                 game.setScreen(game.startScreen);
             }
         });
         uiRoot.add(helpButton).spaceBottom(10).fillX();
-        //uiRoot.row();
     }
 
+    /*
+    designates the given stage to receive and handle all user input events
+     */
     @Override
     public void show() {
         Gdx.input.setInputProcessor(stage);
@@ -125,6 +132,10 @@ public class HelpScreen implements Screen {
 
     }
 
+    /*
+    Disposes stage as well as used textures when no longer needed to avoid
+    memory leak.
+    */
     @Override
     public void dispose() {
         stage.dispose();
