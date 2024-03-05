85fb7865523d4f5781a4feb93cf5735cf1affaba
Mina Tolfsen <minabye@me.com>
2024-03-05 12:41:22 +0100

    Ordent opp i skrive feil i path til "map_2.tmx"

--- src/main/java/inf112/moustachmania/app/utils/Constants.java
+++ src/main/java/inf112/moustachmania/app/utils/Constants.java
@@ -3,7 +3,7 @@ package inf112.moustachmania.app.utils;
 public class Constants {
 
     public static final String skinPath = "assets/skin/uiskin.json";
-    public static final String[] mapPaths = new String[]{"assets/maps/map_1.tmx", "assets/maps/map_1.tmx"};
+    public static final String[] mapPaths = {"assets/maps/map_1.tmx", "assets/maps/map_2.tmx"};
     public static final float scale = 1 / 16f;
 
 
